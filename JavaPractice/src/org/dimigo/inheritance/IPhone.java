5906e156f61547fefe61b4ec3242599839b4983f
WP152401 <smkang444@gmail.com>
2017-05-15 22:31:16 +0900

    Add files via upload

---
+++ JavaPractice/src/org/dimigo/inheritance/IPhone.java
@@ -0,0 +1,34 @@
+/**
+ * 
+ */
+package org.dimigo.inheritance;
+
+/**
+ * <pre>
+ * org.dimigo.inheritance
+ *   |_ Iphone
+ *
+ * 1. 개요 :
+ * 2. 작성일 : 2017. 5. 15.
+ * </pre>
+ *
+ * @author		: smkan
+ * @version		: 1.0
+ */
+public class IPhone extends SmartPhone {
+	public IPhone(){
+		
+	}
+	public IPhone(String model,String company, int price){
+		this.model = model;
+		this.company = company;
+		this.price = price;
+	}
+	public void pay(){
+		System.out.println("애플 페이로 결제합니다.");
+	}
+	public void useAirDrop(){
+		System.out.println("AirDrop 기능을 사용합니다.");
+	}
+	
+}
---
+++ JavaPractice/src/org/dimigo/inheritance/SmartPhone.java
@@ -0,0 +1,60 @@
+/**
+ * 
+ */
+package org.dimigo.inheritance;
+
+/**
+ * <pre>
+ * org.dimigo.inheritance
+ *   |_ SmartPhone
+ *
+ * 1. 개요 :
+ * 2. 작성일 : 2017. 5. 15.
+ * </pre>
+ *
+ * @author		: smkan
+ * @version		: 1.0
+ */
+public class SmartPhone {
+	String model;
+	String company;
+	int price;
+	public SmartPhone(){
+		
+	}
+	/**
+	 * @param model
+	 * @param company
+	 * @param price
+	 */
+	public SmartPhone(String model, String company, int price) {
+		this.model = model;
+		this.company = company;
+		this.price = price;
+	}
+	
+	public void turnon(){
+		System.out.println(model+"의 전원을 켭니다.");
+	}
+	public void turnoff(){
+		System.out.println(model+"의 전원을 끕니다.");
+	}
+	public void pay(){
+		System.out.println(company+" 페이로 결제합니다.");
+	}
+	public void useSpecialFunction(SmartPhone phone){
+		if(phone instanceof IPhone){
+			IPhone a=(IPhone) phone;
+			((IPhone) phone).useAirDrop();
+		} else if (phone instanceof Galaxy){
+			Galaxy b = (Galaxy) phone;
+			((Galaxy) phone).useWirelessCharging();
+		}
+	}
+	
+	public String toString() {
+		return "모델명 : "+model+", 제조사 : "+company+"가격 : "+String.format("%,d", price);
+	}
+	
+	
+}
